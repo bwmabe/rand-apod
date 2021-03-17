268f87323a16b1f4cc7c36c3054219ba66da2fda
bwmabe <9590854+bwmabe@users.noreply.github.com>
2021-03-17 11:25:03 -0400

    rewrite to reflect more python experience

--- randapod.py
+++ randapod.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-import re
 import requests
-from random import *
-from datetime import *
+from bs4 import BeautifulSoup
+from random import randint
+from datetime import datetime
 
 #           ja  fe mr  ap  ma  jn  jl  au  sp  oc  no  de
 max_days = [31, 0, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-base_url = 'https://apod.nasa.gov/apod/'
+base_url = 'https://apod.nasa.gov/apod/ap{:0>2d}{:0>2d}{:0>2d}.html'
 
 date = datetime.now()
 
@@ -15,13 +15,15 @@ current_year = int(date.strftime("%y"))
 current_day = int(date.strftime("%d"))
 current_month = int(date.strftime("%m"))
 
+
 def to_str(data):
     if data < 10:
         return "0" + str(data)
     else:
         return str(data)
 
-def random():
+
+def random_url():
     if randint(0, 100) > 25:
         year = randint(0, current_year)
     else:
@@ -42,22 +44,41 @@ def random():
                 day = randint(1, 29)
             else:
                 day = randint(1, 28)
-                
-    return base_url + 'ap' + to_str(year) + to_str(month) + to_str(day) + '.html'
 
-#print(randapod())
+    return base_url.format(year, month, day)
+
+
+def get_page(url):
+    try:
+        page = requests.get(url)
+        if page.status_code != requests.codes.ok:
+            page.raise_for_status()
+        return page.content
+    except requests.exceptions.HTTPError as e:
+        print(e)
+        exit(1)
+    except KeyboardInterrupt:
+        print("User interrupt")
+        exit(0)
+
+
+def process_page(page):
+    soup = BeautifulSoup(page, 'html.parser')
+
+    title = soup.title.string
+
+    image = soup.find_all("img")[0]['src']
+
+    return(title, "https://apod.nasa.gov/apod/{}".format(image))
+
 
 def randapod():
-    url = random()
-    page = requests.get(url)
+    page = BeautifulSoup(get_page(random_url()), 'html.parser')
+    title = page.title.string
+    image = page.find_all("img")[0]['src']
+    return(title, "https://apod.nasa.gov/apod/{}".format(image))
 
-    title_raw = re.search("\<title\>.*\<\/title\>", str(page.content)).group(0)
-    title = title_raw.partition(">")[2].partition("\\")[0]
-
-    img_raw = re.search("image/.*\"\>", str(page.content)).group(0)
-    img = base_url + img_raw.partition("\"")[0]
-
-    return title + '\n' + img
 
 if __name__ == '__main__':
-    print(randapod())
+    r = randapod()
+    print("Title: %s \nURL: %s" % r)
